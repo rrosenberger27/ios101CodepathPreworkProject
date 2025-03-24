e6d22b561b196334de72b33d2556bc703f2cb9aa
Riley <rileyrosenberger23@gmail.com>
2025-03-24 12:21:11 -0400

    built out project

--- ios101CodepathPreworkProject/ViewController.swift
+++ ios101CodepathPreworkProject/ViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        let randomColor = changeColor()
+        let randomColor = changeColor(alphaVal: 0.2)
         view.backgroundColor = randomColor
     }
-    func changeColor() -> UIColor{
+    func changeColor(alphaVal: CGFloat) -> UIColor{
 
             let red = CGFloat.random(in: 0...1)
             let green = CGFloat.random(in: 0...1)
             let blue = CGFloat.random(in: 0...1)
 
-            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            return UIColor(red: red, green: green, blue: blue, alpha: alphaVal)
         }
+    
+    
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var mySchool: UILabel!
+    @IBOutlet weak var careerGoal: UILabel!
+    
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        let randomColor = changeColor(alphaVal : 0.8)
+        myName.textColor = randomColor
+        mySchool.textColor = randomColor
+        careerGoal.textColor = randomColor
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
